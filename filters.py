be594047a607ed6f4734ad6288c06205534f8f01
unknown <gsolaini@gmail.com>
2019-02-25 10:30:05 +0000

    added x,y image translation and applied it to the mask function

--- filters.py
+++ filters.py
@@ -1,4 +1,5 @@
 from PIL import Image
+from PIL import PpmImagePlugin
 
 def modify_val(image_path, value):
 
@@ -18,17 +19,16 @@ def modify_val(image_path, value):
 			pix[x,y] = tuple(rgb_vals)
 			
 			
-	im.show()  
+	im.show()
 	
-def mask(image_path, mask_path, threshold):
+def mask(image_path, mask_path, mask_translation, threshold):
 
 	# get original image
 	im = Image.open(image_path)
 	pix = im.load()
 	
-	# get image mask
-	mask = Image.open(mask_path)
-	mask_pix = mask.load()
+	# get translated image mask
+	mask_pix = translate_img(mask_path, mask_translation[0], mask_translation[1])
 	
 	# create masked image
 	width, height = im.size
@@ -39,6 +39,35 @@ def mask(image_path, mask_path, threshold):
 	
 	im.show()
 	
+def translate_img(image_path, x_translation, y_translation):
+	# to do: fix horizontal wrapping
+
+	# load image
+	im = Image.open(image_path)
+	pix = im.load()
+	width, height = im.size
+	
+	# translation value in pixels
+	total_translation = y_translation * width + x_translation
+	
+	# independent copy of pixel values
+	old_pix = Image.open(image_path).load()
+	
+	for x in range(width):
+		for y in range(height):
+			current_pixel = y * width + x
+			
+			old_pixel = current_pixel - total_translation
+			
+			if (old_pixel >=0 and old_pixel < width * height): # so long as the old pixel is valid
+				pix[x,y] = old_pix[old_pixel % width, old_pixel / width]
+			else:
+				pix[x,y] = (0,0,0)
+				
+	im.show()
+	
+	return pix
+
 ################	
 #modify_val('images_cropped/10859.ppm', -100)
-mask('images_cropped/10859.ppm', 'images_noise/circle_mask.png', 220)
+mask('images_cropped/10859.ppm', 'images_noise/circle_mask.png', (10, 10), 220)
