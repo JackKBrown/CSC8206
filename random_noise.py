8f9613015cb91dd3666872f55c7b43af0145ddc7
EJ79 <32852605+EJ79@users.noreply.github.com>
2019-02-26 20:45:51 +0000

    Changed to one version, added loop ability, attempted to make code more readable
    -Removed advanced and simple version to replace with one version, the orginial advanced version. Simple version was redundant
    -Added loop parameter, and moved max and min pix val to the function parameters to allow user control
    -Changed the calculation of total noise to it's own part, was simpler with addition of loop parameter and allows improved readability at slight performance loss

--- random_noise.py
+++ random_noise.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+#from PIL import Image
 
 #Initialise an array of size 40x40, with all elements being 120
 #This represents an image of 40px by 40px in a mid-grey colour (assuming 0-255 value range)
@@ -12,17 +13,13 @@ for i in range(40):
 #Function to create random noise for a given image (np array), with parameters for probability of noise
 #for a specific pixel, p, and an amount of noise, am. The amount represents the upperbound of possible noise applied,
 #for each pixel, being randomly chosen from 1 to amount if a pixel has noise applied. 
-#In addition this function allows you to specify the area of the image you wish to apply noise to
+#In addition this function allows you to specify the area of the image you wish to apply noise to.
+#Also provides a boolean parameter loop, if true when calculating the final noisy value, when out of bounds it will loop to
+#the next value
 #The function will also display the total amount of noise applied
-def randomNoise(img, p=1, am=255, x1=-1, x2=-1, y1=-1, y2=-1):
+def randomNoise(img, maxPixVal=255, minPixVal=0, p=0.1, am=255, x1=-1, x2=-1, y1=-1, y2=-1, loop=True):
     #Copy image to new array to allow noise to be added
     imgNoise = np.copy(img)
-        
-    #Running noise total
-    totalNoise = 0
-    #Pixel value range
-    maxPixVal = 255
-    minPixVal = 0
     
     #If x1,x2,y1,y2 not specified, set as 0 and image size respectively (to apply noise to whole image)
     if(x1 < 0):
@@ -37,62 +34,74 @@ def randomNoise(img, p=1, am=255, x1=-1, x2=-1, y1=-1, y2=-1):
     #For each pixel in specified area
     for i in range(y1,y2):
         for j in range(x1,x2):
-            #Generate random float from 0 to 1
-            r = random.uniform(0,1)
-            #If this generated float is less than p, apply noise to this pixel
-            if(r<=p):
-                #Generate amount of noise to be applied
-                noise = random.randint(1,am+1)
+            #Generate random float from 0 to 1, if less than p, apply noise to the pixel
+            if(random.uniform(0,1)<=p):
+                #Generate amount of noise to be applied.
+                noise = random.randint(1,am+1)                
                 #Random number of 0 or 1, 0 represents that noise will be added, 1 represents noise will be subtracted
                 r = random.randint(0,1)
+                
                 #Add noise
-                if(r == 0):
+                if(r == 0):                        
                     #Value of pixel after noise applied
-                    addNoise = img[i,j]+noise
+                    newVal = img[i,j]+noise
                     #If valid pixel value
-                    if(addNoise<=maxPixVal):
+                    if(newVal<=maxPixVal):
                         #Set the new value in imgNoise
-                        imgNoise[i,j] = addNoise
-                        #Add to total noise the value of noise added
-                        totalNoise = totalNoise + noise
+                        imgNoise[i,j] = newVal
                     #Else, the value is above the pixel value range
                     else:
-                        #Set as max value
-                        imgNoise[i,j] = maxPixVal
-                        #Add the effective noise that has been added
-                        totalNoise = totalNoise + (maxPixVal - img[i,j])                        
-                #Subtract noise
+                        #If looping
+                        if(loop):
+                            #New noisy value is the newValue modulo maxPixVal
+                            imgNoise[i,j] = newVal % maxPixVal
+                        #Else, not looping
+                        else:
+                            #Set as max value
+                            imgNoise[i,j] = maxPixVal
+                        
+                #Else subtract noise
                 else:
                     #Calculate noise value after subtraction
-                    subNoise = img[i,j]-noise
+                    newVal = img[i,j]-noise
                     #If valid pixel value
-                    if(subNoise>=minPixVal):
+                    if(newVal>=minPixVal):
                         #Set new value in imgNoise
-                        imgNoise[i,j] = subNoise
-                        #Add the noise that was subtracted to the running total
-                        totalNoise = totalNoise + noise
+                        imgNoise[i,j] = newVal
                     #Else, the value is below the pixel value range
                     else:
-                        #Set as min pixel value
-                        imgNoise[i,j] = minPixVal
-                        #Add the effective noise that was subtracted from original value
-                        totalNoise = totalNoise + (img[i,j]) 
+                        #If looping
+                        if(loop):
+                            #New noisy value is the value of noise after addition
+                            imgNoise[i,j] = 255 - (newVal % maxPixVal)
+                        #Else, not looping
+                        else:
+                            #Set as min pixel value
+                            imgNoise[i,j] = minPixVal
+
+    #Running total for noise applied
+    totalNoise = 0
+    
+    #Below calculates the final effective noise by summing absolute difference for each pixel in
+    #the original image and the noisy image
+    for i in range(y1,y2):
+        for j in range(x1,x2):
+            totalNoise += abs(img[i,j]-imgNoise[i,j])
+    
     
     #Below plots and shows the original, and noise adjusted images.
     plt.imshow(img, cmap='gray', vmin=0, vmax=255)
     plt.title("Original Image")
     plt.show()
     
-    plt.gray()
     plt.imshow(imgNoise, cmap='gray', vmin=0, vmax=255)
     plt.title("Noise Image")
     plt.show()
     
     #Print total noise that was added/subtracted
     print("Total Noise: ", totalNoise)
-	
-	
-#Examples
-randomNoise(grey,1,50) 
 
-randomNoise(grey,1,50,10,30,10,30) 
+
+#Examples
+randomNoise(grey, p=1, am=55555555) 
+
