c7bbcc594e83bc5175acbda3e5b1193710135221
Vaclav <vydrav@u.k2.cz>
2019-03-11 15:30:23 +0100

    quickfix df_inter

--- df_inter.py
+++ df_inter.py
@@ -5,81 +5,80 @@ from PIL import Image
 from keras.models import model_from_json
 from keras import backend as K
 import numpy as np
+import argparse
 
 class Dummy:
     pass
 
 
-def __main__():
-    parser = argparse.ArgumentParser(description='Craft adversarial examples')
-    parser.add_argument('--img',dest='image_path', type=str, default='images_cropped/00000/00000_00000.ppm', help='Image path')
-    parser.add_argument('--cost',dest='min_cost', type=float, default=0.90, help='Minimum certaintity to stop')
-    parser.add_argument('--save',dest='save_as', type=str, default='deepfooled-img.png', help='Where to save the perturbated image')
-    parser.add_argument('--eta',dest='eta', type=float, default=0.01, help='Eta (Learning rate)')
-    parser.add_argument('--noises',dest='noises', type=str, default='deepfool-noise.png', help='Where to save the noise')
-    parser.add_argument('--noises',dest='noises', type=str, default='deepfool-noise_scaled.png', help='Where to save the scaled noise')
 
-    args = parser.parse_args()
+parser = argparse.ArgumentParser(description='Craft adversarial examples')
+parser.add_argument('--img',dest='image_path', type=str, default='images_cropped/00000/00000_00000.ppm', help='Image path')
+parser.add_argument('--cost',dest='min_cost', type=float, default=0.90, help='Minimum certaintity to stop')
+parser.add_argument('--save',dest='save_as', type=str, default='deepfooled-img.png', help='Where to save the perturbated image')
+parser.add_argument('--eta',dest='eta', type=float, default=0.01, help='Eta (Learning rate)')
+parser.add_argument('--noise',dest='noise', type=str, default='deepfool-noise.png', help='Where to save the noise')
+parser.add_argument('--noises',dest='noises', type=str, default='deepfool-noise_scaled.png', help='Where to save the scaled noise')
 
-    sess = tf.InteractiveSession()
-    K.set_session(sess)
-    sess.run(tf.global_variables_initializer())
-    sess.run(tf.local_variables_initializer())
+args = parser.parse_args()
 
-    env = Dummy()
+sess = tf.InteractiveSession()
+K.set_session(sess)
+sess.run(tf.global_variables_initializer())
+sess.run(tf.local_variables_initializer())
 
-    # load json and create model
-    json_file = open('DNN.json', 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    model = model_from_json(loaded_model_json)
-    # load weights into new model
-    model.load_weights("DNN_weights.h5")
+env = Dummy()
 
-    # get the pixels
-    im = image.load_img(args.image_path, target_size=(40,40)) # open the image
-    x = image.img_to_array(im) # extracts the pixels
-    x /= 255.
-    x = np.reshape(x, newshape=(40, 40, 3))
-    x = np.expand_dims(x, axis=0)
+# load json and create model
+json_file = open('DNN.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+model = model_from_json(loaded_model_json)
+# load weights into new model
+model.load_weights("DNN_weights.h5")
 
-    env.adv_epochs = tf.placeholder(tf.int32, (), name='adv_epochs')
-    env.x = tf.placeholder(tf.float32, (None, 40, 40, 3), name='x')
+# get the pixels
+im = image.load_img(args.image_path, target_size=(40,40)) # open the image
+x = image.img_to_array(im) # extracts the pixels
+x /= 255.
+x = np.reshape(x, newshape=(40, 40, 3))
+x = np.expand_dims(x, axis=0)
 
-    xadv = deepfool(model, env.x, eta=args.eta, epochs=3, batch=False, clip_min=0.0, clip_max=1.0, min_prob=0.0)
+env.adv_epochs = tf.placeholder(tf.int32, (), name='adv_epochs')
+env.x = tf.placeholder(tf.float32, (None, 40, 40, 3), name='x')
 
-    adv = sess.run(xadv, feed_dict={env.x: x, env.adv_epochs: 3})
+xadv = deepfool(model, env.x, eta=args.eta, epochs=3, batch=False, clip_min=0.0, clip_max=1.0, min_prob=0.0)
 
-    # print(adv.shape)
-    # print(adv)
-    print(model.predict(adv))
+adv = sess.run(xadv, feed_dict={env.x: x, env.adv_epochs: 3})
 
-    _preds = model.predict(adv)[0]
-    true_class = list(_preds).index(np.amax(_preds))
-    print('New class: ' + str(true_class))
+# print(adv.shape)
+# print(adv)
+print(model.predict(adv))
 
-    adv = adv[0]
-    noise = x[0] - adv
+_preds = model.predict(adv)[0]
+true_class = list(_preds).index(np.amax(_preds))
+print('New class: ' + str(true_class))
 
-    adv *= 255
-    # Save the hacked image!
-    im = Image.fromarray(adv.astype(np.uint8))
-    im.save(args.save)
+adv = adv[0]
+noise = x[0] - adv
 
-    # Save the hacked image!
-    im = Image.fromarray(noise.astype(np.uint8))
-    im.save(args.noise)
+adv *= 255
+# Save the hacked image!
+im = Image.fromarray(adv.astype(np.uint8))
+im.save(args.save_as)
 
-    _min = np.amin(noise)
-    _max = np.amax(noise)
-    scaled_noise = np.array(list(map(lambda x: ((x -_min) / abs(_max  - _min)) * 255, noise)))
+# Save the hacked image!
+im = Image.fromarray(noise.astype(np.uint8))
+im.save(args.noise)
 
-    im = Image.fromarray(scaled_noise.astype(np.uint8))
-    im.save(args.noises)
+_min = np.amin(noise)
+_max = np.amax(noise)
+scaled_noise = np.array(list(map(lambda x: ((x -_min) / abs(_max  - _min)) * 255, noise)))
 
-    pert = np.sum(np.abs(noise))
-    print('Total pert: ' + str(pert))
-    sess.close()
+im = Image.fromarray(scaled_noise.astype(np.uint8))
+im.save(args.noises)
 
-if __name__=='__main__':
-    main()
+pert = np.sum(np.abs(noise))
+print('Total pert: ' + str(pert))
+sess.close()
+
